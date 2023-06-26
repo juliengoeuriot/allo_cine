7056ffbb932f5060b762e50bd93a8f308ec2a5e0
juliengoeuriot <goeuriotj@gmail.com>
2023-06-26 14:17:45 +0200

    Initial commit

---
+++ src/service/PDOService.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace App\Service;
+
+class PDOService
+{
+protected PDOService $pdo;
+
+private string $dsn = 'mysql:host=127.0.0.1:3306;dbname=allo_cine';
+private string $user = 'root';
+private string $pwd = '';
+}
+
+?>
